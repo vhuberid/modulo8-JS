7365300ef2c43f2f1205f16a28db82edf3bdd9f4
vhuberid <vanessa.idalencio@hotmail.com>
2022-12-14 12:27:20 +0100

    conteudos aulas

---
+++ functions-arguments-and-parameters.js
@@ -0,0 +1,9 @@
+//Funcos - Argumentos (ao chamar a funcao) e Parametros(dentro da funcao)
+
+function sum (number1, number2) {       //sum = soma; parametros dentro do parentesis podem ser quantos forem, sem numero limite.
+    console.log(number1+number2)        //quando a funcao for incovada/chamada, os valores precisam ser declarados
+}
+
+sum (2,3)                               //invoke/call a funcao; dentro do parentesis estao os argumentos, o que seria antes a const/let
+sum (5,10)                              //a funcao em si nao tem argumentos definidos; pode definir mais vezes ao chamar a funcao
+
---
+++ functions.js
@@ -0,0 +1,9 @@
+// Funcoes - Functions
+
+function functionNameAboutWhatIsBeingDone () {                     //funcao só executa quando ela é CHAMADA
+    console.log("Conteudo da Funcao")
+}
+
+functionNameAboutWhatIsBeingDone()                                  //aqui a funcao vai ser executada, sem isso nao! 
+
+//funcao permite reexecutar um código. Sempre que quiser utilizar este mesmo código, só precisa chamar a funcao novamente.
